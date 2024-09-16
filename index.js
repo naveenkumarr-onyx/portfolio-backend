8200606ccc3caf8ffaa861c868e47b4c4fc58a44
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-09-16 22:51:12 +0530

    added file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import router from "./routes/router.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+connectDB();
+const PORT = process.env.PORT || 3000;
+app.get("/api/portfolio", (req, res) => {
   res.send("Hello World");
 });
+app.use("/api/portfolio", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
