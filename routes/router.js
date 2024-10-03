25d99036e7f7de41f18322f5c020badb9277020e
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-10-03 23:22:23 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,15 @@
 import express from "express";
-import Intro from "../models/portfolio.js";
+import { Intro, Experience } from "../models/portfolio.js";
 
 const router = express.Router();
 
 router.get("/get-data", async (req, res) => {
   try {
     const intros = await Intro.find();
+    const experience = await Experience.find();
     res.status(200).send({
       intro: intros[0],
+      experience: experience,
     });
   } catch (error) {
     res.status(500).json({
@@ -28,5 +30,30 @@ router.post("/add-intro", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.post("/add-experience", async (req, res) => {
+  const experience = new Experience({
+    company: req.body.company,
+    location: req.body.location,
+    position: req.body.position,
+    period: req.body.period,
+    mode: req.body.mode,
+    description: req.body.description || null,
+  });
+  try {
+    await experience.save();
+    res.status(201).json({
+      data: experience,
+      success: true,
+      message: "experience updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+router.put();
 
 export default router;
