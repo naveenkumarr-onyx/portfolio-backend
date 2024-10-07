d779607f5b6f97e8efcd6d3fcf4ddf8992f6e3ac
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-10-07 20:46:04 +0530

    added projects

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Intro, Experience } from "../models/portfolio.js";
+import { Intro, Experience, Projects } from "../models/portfolio.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get("/get-data", async (req, res) => {
   try {
     const intros = await Intro.find();
     const experience = await Experience.find();
+    const projects = await Projects.find();
     res.status(200).send({
       intro: intros[0],
       experience: experience,
@@ -38,6 +39,7 @@ router.post("/add-experience", async (req, res) => {
     position: req.body.position,
     period: req.body.period,
     mode: req.body.mode,
+    active: req.body.active || false,
     description: req.body.description || null,
   });
   try {
@@ -80,6 +82,34 @@ router.put("/update-experience/:id", async (req, res) => {
     res.status(500).json({
       message: error.message,
       success: false,
+    });
+  }
+});
+
+router.post("/update-projects", async (req, res) => {
+  // Check if req.body.technologies is a string and split it into an array
+  const technologiesArray = Array.isArray(req.body.technologies)
+    ? req.body.technologies
+    : req.body.technologies.split(",").map((tech) => tech.trim());
+
+  const pro = new Projects({
+    title: req.body.title,
+    technologies: technologiesArray,
+    project_image: req.body.project_image,
+    github_link: req.body.github_link,
+    live_link: req.body.live_link,
+  });
+
+  try {
+    await pro.save();
+    res.status(201).json({
+      data: pro,
+      success: true,
+      message: "Projects updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
   }
 });
