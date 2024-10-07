2a7e1b1b7b295632585b8e8dfb92daa9f26824e0
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-10-07 20:45:47 +0530

    Update portfolio.js

--- models/portfolio.js
+++ models/portfolio.js
@@ -32,12 +32,39 @@ const experienceSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  active: {
+    type: Boolean,
+  },
   description: {
     type: String,
   },
 });
 
+const projectsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  technologies: {
+    type: Array,
+    required: true,
+  },
+  project_image: {
+    type: String,
+    required: true,
+  },
+  github_link: {
+    type: String,
+    required: true,
+  },
+  live_link: {
+    type: String,
+    required: true,
+  },
+});
+
 const Intro = mongoose.model("Intro", introSchema);
 const Experience = mongoose.model("Experience", experienceSchema);
+const Projects = mongoose.model("Projects", projectsSchema);
 
-export { Intro, Experience };
+export { Intro, Experience, Projects };
