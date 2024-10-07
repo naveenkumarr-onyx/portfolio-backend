1130ed4e88e37cac9e315ca40daa71f7866c4a29
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-10-07 20:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ import router from "./routes/router.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-app.use(cors("*"));
+
+app.use(
+  cors({
+    origin: "*", // Allow all origins for development purposes
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
+
 app.use(express.json());
+
 connectDB();
+
 const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Backend working properly!!!");
 });
+
 app.use("/api/portfolio", router);
 
 app.listen(PORT, () => {
