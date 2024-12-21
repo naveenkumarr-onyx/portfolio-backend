efe15a60b4a6c165ad9e19dc1f417f052f1e5d7d
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-12-21 18:18:02 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-  const dbUrl = process.env.MONGO_URI;
+  const dbUrl = process.env.MONGODB_URI;
 
   if (!dbUrl) {
     console.error("MongoDB URI is undefined. Check your .env file.");
@@ -10,7 +10,7 @@ export const connectDB = () => {
 
   mongoose
     .connect(dbUrl)
-    .then(() => console.log("MongoDB connection established successfully"))
+    .then(() => console.log(`MongoDB connection established successfully ${dbUrl}`))
     .catch((error) => {
       console.error(`MongoDB connection error: ${error.message}`);
       process.exit(1); // Exit with failure
